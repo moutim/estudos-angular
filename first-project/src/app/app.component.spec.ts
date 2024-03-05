c1e902ed56f02e83e49a4eeafd4f71e77acbdbb8
Vitor Moutim <moutimg@gmail.com>
2024-03-05 20:47:34 -0300

    fix: cria projeto modularizado

--- first-project/src/app/app.component.spec.ts
+++ first-project/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projeto-estudos' title`, () => {
+  it(`should have as title 'first-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-estudos');
+    expect(app.title).toEqual('first-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, projeto-estudos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-project');
   });
 });
